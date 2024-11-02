7220f154d9444737bc3cc98e226e1346b42ca08f
M0BAZ <132110139+M0BAZ@users.noreply.github.com>
2024-11-03 00:14:26 +0300

    Add files via upload

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "editSubmissionPage") {
     console.log("Перешли на страницу с шаблоном 'action=editsubmission'");
@@ -7,7 +6,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     submitButton.addEventListener('click', function (){ 
 
     })
-    
+ 
   } else if (message.action === "viewPage") {
     console.log("Перешли на страницу с шаблоном 'action=view'");
     
@@ -35,7 +34,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         formData.append('file', new Blob([fileBlob]), file_name); // Добавляем файл в FormData
       
         try {
-          const uploadResponse = await fetch('http://<YOUR_SERVER_URL>/upload', { // add adress !!!!!!!!!!!!!!!!!!!!!!!
+          const uploadResponse = await fetch('https://file-downloader-1lg9.onrender.com', {
             method: 'POST',
             body: formData
           });
@@ -51,60 +50,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   sendResponse({ status: "Сообщение обработано в content.js" });
 }
 });
-
-=======
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "editSubmissionPage") {
-    console.log("Перешли на страницу с шаблоном 'action=editsubmission'");
-
-    const submitButton = document.getElementById("id_submitbutton");
-    submitButton.addEventListener('click', function (){ 
-
-    });
-    
-  } else if (message.action === "viewPage") {
-    console.log("Перешли на страницу с шаблоном 'action=view'");
-    
-    const table = document.querySelector('.submissionstatussubmitted.cell.c1.lastcol').textContent;
-  
-    if (table == "Отправлено для оценивания"){
-      const fileElement = document.querySelectorAll('.fileuploadsubmission a');
-      
-      const fileUrl = fileElement[fileElement.length-1].getAttribute("href");
-      
-      chrome.runtime.sendMessage({ action: "getCookies" }, async (cookieString) => {
-        // Загружаем файл с заголовками и cookies
-        const response = await fetch(fileUrl, {
-          method: "GET",
-          headers: {
-            "Cookie": cookieString
-          }
-        });
-
-        const fileBlob = await response.blob();
-        const fileElement_2 = document.querySelectorAll(".fileuploadsubmission img");
-        const file_name = fileElement_2[fileElement_2.length-1].getAttribute("title");
-        
-        // Создаем объект FormData и добавляем файл
-        const formData = new FormData();
-        formData.append("file", fileBlob, file_name);
-
-        // Отправляем файл на сервер
-        fetch("https://file-downloader-es32.onrender.com", {
-          method: "POST",
-          body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-          console.log("Файл успешно загружен на сервер:", data);
-        })
-        .catch(error => {
-          console.error("Ошибка при загрузке файла:", error);
-        });
-      });
-    }
-
-    sendResponse({ status: "Сообщение обработано в content.js" });
-  }
-});
->>>>>>> 969af0d76c3313bded7443e1d7cf3e07fc130265
